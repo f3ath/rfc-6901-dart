a588a62f1180e9b51b58e7442c212ada5685a222
nullrocket <douglas.b.hill@gmail.com>
2022-03-01 00:41:17 -0500

    Added assertions for hashCode to tests.

--- test/equality_test.dart
+++ test/equality_test.dart
@@ -13,5 +13,9 @@ void main() {
     expect(thisPointer == thatPointer, true);
     expect(emptyPointer == explicitEmptyPointer, true);
     expect(thisPointer != differentPointer, true);
+
+    expect(thisPointer.hashCode == thatPointer.hashCode, true);
+    expect(emptyPointer.hashCode == explicitEmptyPointer.hashCode, true);
+    expect(thisPointer.hashCode != differentPointer.hashCode, true);
   });
 }
